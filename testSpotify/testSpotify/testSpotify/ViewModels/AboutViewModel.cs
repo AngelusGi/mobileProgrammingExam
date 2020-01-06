f98308e98a8bc8fd761dd1f24e6c2a0c88b66abd
Angelo Gino Varrati <angelo.varrati@outlook.it>
2020-01-06 18:29:19 +0100

    musxmatch in about

--- testSpotify/testSpotify/testSpotify/ViewModels/AboutViewModel.cs
+++ testSpotify/testSpotify/testSpotify/ViewModels/AboutViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Windows.Input;
 using Xamarin.Essentials;
 using Xamarin.Forms;
@@ -6,21 +7,40 @@ using SpotifyAPI.Web;
 using SpotifyAPI.Web.Auth;
 using SpotifyAPI.Web.Enums;
 using SpotifyAPI.Web.Models;
+using Plugin.Toast;
+using MusixMatch_API;
+using MusixMatch_API.APIMethods.Artist;
+
 namespace testSpotify.ViewModels
 {
     public class AboutViewModel : BaseViewModel
     {
+        string res = string.Empty;
         public AboutViewModel()
         {
             Title = "About";
-            TestAuthCommand = new Command( () =>  TestAuth());
+            TestAuthCommand = new Command(() => TestAuth());
         }
 
         public ICommand TestAuthCommand { get; }
 
         private void TestAuth()
         {
-            
+
+            var api = new MusixMatchApi("5f18a4bfea8c334574b0860a8b638409");
+            var artistSearch = new ArtistSearch { ArtistName = "Paolo Nutini" };
+            //var lyricsSearch = new MusixMatch_API.APIMethods.Track.TrackSearch { FilterOnArtistId = artistSearch.MusicBrainzArtistId };
+            api.ArtistSearch(artistSearch, result =>
+            {
+                // Your results in result
+                res = result.
+            }, error =>
+            {
+                // Something went wrong. Error is in error string.
+            });
+
+            CrossToastPopUp.Current.ShowToastMessage(res);
+
         }
     }
 }
